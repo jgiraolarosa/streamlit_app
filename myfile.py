8a58480809c7d033e6f8c5f40173db351d3327da
jgiraolarosa <140277335+jgiraolarosa@users.noreply.github.com>
2023-07-22 16:39:10 -0500

    Add files via upload

--- myfile.py
+++ myfile.py
@@ -7,14 +7,13 @@ st.title("Título del proyecto Jorge 2")
 st.write("Hola, ¿**cómo** estás?")
 st.latex("\omega=x_2")
 
+#Lectura del dataframe
+df = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
+
+#Magnitud
 magnitudInicio = st.slider("Magnitud inicio:", 0, 9, step=1)
 magnitudFin = st.slider("Magnitud fin:", magnitudInicio, 9, step=1)
-st.write("La magnitud es {}".format(magnitudInicio))
-st.write("La magnitud es {}".format(magnitudFin))
+df.query("MAGNITUD >= {}".format(magnitudInicio))
 
-appointment = st.slider("Programe la asesoria:", value=(time(11, 30), time(12, 45)))
-st.write("Esta agendado para:", appointment)
-
-df2 = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
-#df2 = df2.loc(df['MAGNITUD'] )
-st.map(df2)
+#Mostrar mapa
+st.map(df)
