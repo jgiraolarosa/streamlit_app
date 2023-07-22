7e475db89f6c258734514ecad09ebaa5ba8c1f63
jgiraolarosa <140277335+jgiraolarosa@users.noreply.github.com>
2023-07-22 16:32:06 -0500

    Add files via upload

--- myfile.py
+++ myfile.py
@@ -7,8 +7,8 @@ st.title("Título del proyecto Jorge 2")
 st.write("Hola, ¿**cómo** estás?")
 st.latex("\omega=x_2")
 
-num = st.slider("num", 0, 100, step=1)
-st.write("El número ingresado es {}".format(num))
+magnitud = st.slider("Magnitud:", 0, 9, step=1)
+st.write("La magnitud es {}".format(magnitud))
 
 appointment = st.slider("Programe la asesoria:", value=(time(11, 30), time(12, 45)))
 st.write("Esta agendado para:", appointment)
@@ -17,12 +17,6 @@ n = st.slider("n", 5,100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
 st.line_chart(chart_data)
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(df)
-
-#df2 = pd.read_excel('C:/Pucp/IA/Catalogo1960_2021.xlsx')
 df2 = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
+#df2 = df2.loc(df['MAGNITUD'] )
 st.map(df2)
