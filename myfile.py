a65d5901b4cda47f15e90aafdbeedb0ba478e332
Johan-hub75 <69058509+Johan-hub75@users.noreply.github.com>
2023-07-22 16:57:06 -0500

    comentario de prueba

--- myfile.py
+++ myfile.py
@@ -9,6 +9,9 @@ st.write("Hola, ¿**cómo** estás?")
 #Lectura del dataframe
 df = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
 
+
+
+
 #Magnitud
 magnitudInicio = st.slider("Magnitud inicio:", 3, 9, step=1)
 magnitudFin = st.slider("Magnitud fin:", magnitudInicio, 9, step=1)
