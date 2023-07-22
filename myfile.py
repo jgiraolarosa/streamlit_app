554d2b5b0fc0a9b95090bf184dce445a87609b76
jgiraolarosa <140277335+jgiraolarosa@users.noreply.github.com>
2023-07-22 16:34:30 -0500

    Add files via upload

--- myfile.py
+++ myfile.py
@@ -7,7 +7,7 @@ st.title("Título del proyecto Jorge 2")
 st.write("Hola, ¿**cómo** estás?")
 st.latex("\omega=x_2")
 
-magnitud = st.slider("Magnitud:", 0, 9, step=0.1)
+magnitud = st.slider("Magnitud:", 0, 9, step=1)
 st.write("La magnitud es {}".format(magnitud))
 
 appointment = st.slider("Programe la asesoria:", value=(time(11, 30), time(12, 45)))
