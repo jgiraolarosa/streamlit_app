76979424a074e0dfede2384ec3e717680c0daa48
jgiraolarosa <140277335+jgiraolarosa@users.noreply.github.com>
2023-07-22 16:47:01 -0500

    Add files via upload

--- myfile.py
+++ myfile.py
@@ -10,7 +10,7 @@ st.write("Hola, ¿**cómo** estás?")
 df = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
 
 #Magnitud
-magnitudInicio = st.slider("Magnitud inicio:", 0, 9, step=1)
+magnitudInicio = st.slider("Magnitud inicio:", 3, 9, step=1)
 magnitudFin = st.slider("Magnitud fin:", magnitudInicio, 9, step=1)
 queryMagnitud = "MAGNITUD >= " + str(magnitudInicio) + " and MAGNITUD <= " + str(magnitudFin)
 #df = df.query("MAGNITUD >= {}".format(magnitudInicio))
