df3d803aaf80f46e57d0ad9b5c58e4aa098c2efe
Johan-hub75 <69058509+Johan-hub75@users.noreply.github.com>
2023-07-25 07:21:29 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -30,7 +30,7 @@ st.write("Fecha seleccionada:", start_time)
 queryTime = "FECHA_UTC >= " + str(start_time)
 
 
-df = df[(df.MAGNITUD>=magnitudInicio) & (df.FECHA_UTC>=start_time]
+df = df[(df.MAGNITUD>=magnitudInicio)]
 #df = df[(df.MAGNITUD>=magnitudInicio) & (df.FECHA_UTC>=start_time]
 #df = df.query(queryTime)
 
