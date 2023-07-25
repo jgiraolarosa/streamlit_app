c48bd42f1f0ba110d90b8af63baf28ca45921bc9
Johan-hub75 <69058509+Johan-hub75@users.noreply.github.com>
2023-07-24 20:54:27 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,6 +7,8 @@ st.title("Título del proyecto Jorge 2")
 st.write("Hola, ¿**cómo** estás?")
 st.write("Bien")
 st.write("Hola, ¿**cómo** estás?")
+st.write("Hola, ¿**cómo** estás?")
+
 
 #Lectura del dataframe
 df = pd.read_excel('/app/streamlit_app/Catalogo1960_2021.xlsx')
