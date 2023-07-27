796b35efd80dcb7cae0bb8f80a284ee326a75021
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-07-27 07:17:03 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -30,11 +30,12 @@ def iguala_formato(fecha_numero):
     dt_object = datetime(int(string[:4]), int(string[4:6]), int(string[6:]))
     return dt_object.date()
 
-def format_time(input_str):
+def format_time(hora):
+    string = str(hora)
     # Extracting hours, minutes, and seconds from the input string
-    hours = int(input_str[:2])
-    minutes = int(input_str[2:4])
-    seconds = int(input_str[4:6])
+    hours = int(string[:2])
+    minutes = int(string[2:4])
+    seconds = int(string[4:6])
 
     # Formatting the time as "HH:MM:SS"
     formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
